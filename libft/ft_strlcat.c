a7037565a062ec06de0e79b4efe02382da9cffdc
jgil-cam <jgil-cam@student.42malaga.com>
2022-04-28 11:18:52 +0200

    copia

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,17 +6,17 @@
 /*   By: jgil-cam <jgil-cam@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 18:46:07 by jgil-cam          #+#    #+#             */
-/*   Updated: 2022/04/27 20:15:32 by jgil-cam         ###   ########.fr       */
+/*   Updated: 2022/04/28 11:15:40 by jgil-cam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t c;
-	size_t d;
-	
+	size_t	c;
+	size_t	d;
+
 	if (dstsize <= ft_strlen(dst))
 		return (dstsize + ft_strlen(src));
 	c = ft_strlen(dst);
@@ -31,13 +31,13 @@ size_t ft_strlcat(char *dst, const char *src, size_t dstsize)
 	return (ft_strlen(dst) + ft_strlen(&src[d]));
 }
 
-int main()
+/*int main()
 {
 	char	strdst [100] = "pepe";
 	char	strsrc [100] = "toalla";
 	
 	printf("Antes: %s\n", strdst);
-	ft_strlcat(strdst, strsrc, ft_strlen(strdst));
+	ft_strlcat(strdst, strsrc, sizeof(strdst));
 	printf("Después: %s\n", strdst);
 	return (0);
-}
+}*/
